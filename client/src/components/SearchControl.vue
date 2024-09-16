d4833bcfb036f5633718c485a06c4f3afdaab7f2
rulkimi <azrulkimi00@gmail.com>
2024-09-16 11:53:20 +0800

    separate the search control

---
+++ client/src/components/SearchControl.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-gray-400 p-6 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">Hello HeklkioGE kkie</div>
+</template>
--- client/src/layout/BaseLayout.vue
+++ client/src/layout/BaseLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
-
+import SearchControl from '../components/SearchControl.vue';
 </script>
 
 <template>
   <div class="w-screen h-screen relative">
-    <div class="fixed top-2 left-2 bg-gray-400 p-6 rounded-lg z-[10000] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">Hello HeklkioGE kkie</div>
+    <div class="fixed top-2 left-2 z-[9999]">
+      <SearchControl />
+    </div>
     <slot></slot>
   </div>
 </template>
