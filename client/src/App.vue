e84285d7d829fa012898e4c68661b68662c52053
rulkimi <azrulkimi00@gmail.com>
2024-09-17 17:08:18 +0800

    handle loading state when searching

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,8 @@ import { ref } from 'vue';
 
 const busMap = ref(null);
 const buses = ref([]);
+const currentRoute = ref('');
+const loading = ref(false);
 
 const setMapView = ({ latitude, longitude }) => {
   console.log('here')
@@ -16,7 +18,9 @@ const setMapView = ({ latitude, longitude }) => {
   busMap.value.openMarkerPopup(latitude, longitude);
 }
 
-const fetchLocation = async routeId => {
+const fetchLocation = async (routeId) => {
+  currentRoute.value = routeId;
+  loading.value = true;
   try {
     const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/vehicle/${routeId}`);
     const responseData = await response.json();
@@ -27,7 +31,7 @@ const fetchLocation = async routeId => {
   } catch (error) {
     console.error(error);
   } finally {
-    // this.loading = false;
+    loading.value = false;
   }
 }
 </script>
@@ -35,9 +39,17 @@ const fetchLocation = async routeId => {
 <template>
   <BaseLayout>
     <template #top-left>
-      <div class="flex flex-col gap-6 bg-gray-400 p-6 h-full rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 hover:bg-opacity-50 transition-all duration-300">
+      <div class="flex flex-col gap-4 bg-gray-400 p-6 h-full rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 hover:bg-opacity-50 transition-all duration-300">
         <SearchControl @route-id="fetchLocation" />
-        <BusList v-if="buses.length" :buses="buses" @bus-selected="setMapView" />
+        <div v-if="currentRoute && loading" class="mt-2 animate-pulse">
+          Searching for <font-awesome-icon class="text-blue-500" :icon="['fas', 'route']" /> <span class="text-blue-500">{{ currentRoute }}</span>
+        </div>
+        <template v-if="!loading">
+          <div v-if="currentRoute" class="mt-2">
+            <font-awesome-icon class="text-blue-500" :icon="['fas', 'route']" /> <span class="text-blue-500">{{ currentRoute }}</span> Buses:
+          </div>
+          <BusList v-if="buses.length" :buses="buses" @bus-selected="setMapView" />
+        </template>
       </div>
     </template>
     <BusMap :buses="buses" ref="busMap" />
--- client/src/layout/BaseLayout.vue
+++ client/src/layout/BaseLayout.vue
@@ -1,7 +1,3 @@
-<script setup>
-import SearchControl from '../components/SearchControl.vue';
-</script>
-
 <template>
   <div class="w-screen h-screen relative">
     <div class="fixed top-2 left-2 z-[9999] w-[400px]">
