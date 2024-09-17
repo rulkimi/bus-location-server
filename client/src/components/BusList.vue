303eff555a6c8dfe1444039d63afc20cb8725237
rulkimi <azrulkimi00@gmail.com>
2024-09-17 16:28:42 +0800

    change the popup design

--- client/src/components/BusList.vue
+++ client/src/components/BusList.vue
@@ -8,19 +8,22 @@
       
       <!-- Bus Vehicle ID -->
       <div class="flex justify-between">
-        <div class="flex gap-4 items-center">
+        <div class="flex gap-2 items-center">
           <font-awesome-icon class="text-blue-500" :icon="['fas', 'bus-simple']" />
           <div class="text-lg font-semibold">
             {{ bus.vehicle_id }}
           </div>
         </div>
-        <span>{{ new Date(bus.timestamp * 1000).toLocaleTimeString('en-US', { timeZone: 'Asia/Kuala_Lumpur', hour: '2-digit', minute: '2-digit', hour12: true }) }}</span>
+        <div class="flex items-center gap-2">
+          <font-awesome-icon class="text-gray-500" :icon="['fas', 'clock']" />
+          <span>{{ new Date(bus.timestamp * 1000).toLocaleTimeString('en-US', { timeZone: 'Asia/Kuala_Lumpur', hour: '2-digit', minute: '2-digit', hour12: true }) }}</span>
+        </div>
       </div>
 
       <!-- Location with FontAwesome Icon -->
-      <div class="text-gray-500 flex items-start gap-4">
+      <div class="text-gray-500 flex items-start gap-2">
         <div>
-          <font-awesome-icon class="text-red-500" :icon="['fas', 'map-marker-alt']" />
+          <font-awesome-icon class="text-red-500" :icon="['fas', 'location-dot']" />
         </div>
         <span>{{ bus.location }}</span>
       </div>
--- client/src/components/BusMap.vue
+++ client/src/components/BusMap.vue
@@ -5,19 +5,55 @@
       v-for="bus in buses"
       :key="bus.vehicle_id"
       :lat-lng="[bus.latitude, bus.longitude]"
-      @click="onMarkerClick(bus.latitude, bus.longitude)"
       ref="markers"
     >
       <l-popup>
-        <div>
-          <b>Vehicle ID:</b> {{ bus.vehicle_id }}<br>
-          <b>Route ID:</b> {{ bus.route_id }}<br>
-          <b>Timestamp:</b> {{ bus.timestamp }}<br>
-          <b>Location:</b> {{ bus.location }}
+        <div class="py-2 flex flex-col gap-2 text-lg">
+          <!-- Vehicle ID -->
+          <div class="flex justify-between text-sm text-gray-400">
+            <div class="flex items-center gap-2">
+              <!-- <font-awesome-icon class="text-blue-500" :icon="['fas', 'bus-simple']" /> -->
+              <span>{{ bus.route_id }} ({{ bus.vehicle_id }})</span>
+            </div>
+            <!-- Route ID -->
+            <!-- <div class="flex items-center gap-2">
+              <font-awesome-icon class="text-green-500" :icon="['fas', 'route']" />
+              <span>{{ bus.route_id }}</span>
+            </div> -->
+            <!-- Timestamp -->
+            <div class="flex items-center gap-2">
+              <!-- <font-awesome-icon class="text-gray-500" :icon="['fas', 'clock']" /> -->
+              <span class="text-nowrap">{{ new Date(bus.timestamp * 1000).toLocaleTimeString('en-US', { timeZone: 'Asia/Kuala_Lumpur', hour: '2-digit', minute: '2-digit', hour12: true }) }}</span>
+            </div>
+          </div>
+
+          <!-- Location -->
+          <div class="flex items-start gap-2 col-span-3">
+            <!-- <div>
+              <font-awesome-icon class="text-red-500" :icon="['fas', 'map-marker-alt']" />
+            </div> -->
+            <span>{{ bus.location }}</span>
+          </div>
+
+          <!-- waze and google map -->
+          <div class="flex gap-4">
+            <div class="flex items-start gap-2 cursor-pointer hover:underline" @click="openWaze(bus.latitude, bus.longitude)">
+              <div>
+                <font-awesome-icon :icon="['fab', 'waze']" />
+              </div>
+              <span>Waze</span>
+            </div>
+            <div class="flex items-center gap-2 cursor-pointer hover:underline" @click="openGoogleMap(bus.latitude, bus.longitude)">
+              <div>
+                <img src="../assets/google-map.png" width="12" alt="">
+              </div>
+              <span>Google Map</span>
+            </div>
+          </div>
         </div>
       </l-popup>
     </l-marker>
-    <l-control-zoom position="topright"  ></l-control-zoom>
+    <l-control-zoom position="topright"></l-control-zoom>
   </l-map>
 </template>
 
@@ -71,11 +107,14 @@ export default {
         console.error('Marker instance or leafletObject not found:', markerInstance);
       }
     },
-    onMarkerClick(latitude, longitude) {
-      const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    openWaze(latitude, longitude) {
       const wazeUrl = `https://waze.com/ul?ll=${latitude},${longitude}&navigate=yes`;
+      window.open(wazeUrl, '_blank');
+    },
+    openGoogleMap(latitude, longitude) {
+      const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
       window.open(googleMapsUrl, '_blank');
-    }
+    },
   }
 };
 </script>
