6bf4580a3b9a4fc2b3828da3a588ec7671330a76
rulkimi <azrulkimi00@gmail.com>
2024-10-18 20:59:37 +0800

    remove unnecessary comments

--- server/app/routes.py
+++ server/app/routes.py
@@ -5,35 +5,27 @@ from app.utils import fetch_gtfs_realtime_feed, reverse_geocode, sort_key, remov
 
 router = APIRouter()
 
-# Sample GTFS-R URLs
+# GTFS-R URLs
 FEEDER_BUS_URL = 'https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-mrtfeeder'
 RAPID_KL_URL = 'https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-kl'
 
-
-@router.get("/")
-def read_root():
-    return {"message": "Welcome to the Bus Location API"}
-
 @router.get("/routes")
 def get_routes():
+
     print("Fetching feeder bus and Rapid KL bus feeds")
     
-    # Fetch feeds for feeder buses and Rapid KL buses
     feeder_bus_content = fetch_gtfs_realtime_feed(FEEDER_BUS_URL)
     rapid_kl_content = fetch_gtfs_realtime_feed(RAPID_KL_URL)
 
-    # Check if feeds were fetched successfully
     if not feeder_bus_content and not rapid_kl_content:
         print("Failed to fetch both feeder bus and Rapid KL feeds.")
         raise HTTPException(status_code=500, detail="Failed to fetch bus data.")
 
-    # Process feeder buses
     feeder_routes = remove_duplicates(sorted(process_bus_feed(feeder_bus_content), key=sort_key))
     rapid_kl_routes = remove_duplicates(sorted(process_bus_feed(rapid_kl_content), key=sort_key))
 
     print(f"Found {len(feeder_routes)} unique feeder routes and {len(rapid_kl_routes)} unique Rapid KL routes.")
     
-    # Return both lists separately, sorted and with duplicates removed
     return {
         "feeder_bus_active_routes": feeder_routes,
         "rapid_kl_active_routes": rapid_kl_routes
@@ -41,9 +33,9 @@ def get_routes():
 
 @router.get("/vehicle/{route_id}")
 def get_vehicle_by_route(route_id: str):
+
     print(f"Searching for vehicles on route: {route_id}")
     
-    # Fetch feeds from both sources
     feeder_bus_content = fetch_gtfs_realtime_feed(FEEDER_BUS_URL)
     rapid_kl_content = fetch_gtfs_realtime_feed(RAPID_KL_URL)
 
@@ -53,21 +45,18 @@ def get_vehicle_by_route(route_id: str):
 
     vehicles = []
 
-    # Process feeder bus feed
     if feeder_bus_content:
         print("Processing feeder bus vehicles")
         feeder_feed = gtfs_realtime_pb2.FeedMessage()
         feeder_feed.ParseFromString(feeder_bus_content)
         vehicles.extend([MessageToDict(entity.vehicle) for entity in feeder_feed.entity])
 
-    # Process rapid KL feed
     if rapid_kl_content:
         print("Processing Rapid KL vehicles")
         rapid_kl_feed = gtfs_realtime_pb2.FeedMessage()
         rapid_kl_feed.ParseFromString(rapid_kl_content)
         vehicles.extend([MessageToDict(entity.vehicle) for entity in rapid_kl_feed.entity])
 
-    # Extract relevant vehicle data
     vehicles_on_route = []
     for vehicle in vehicles:
         vehicle_route_id = vehicle.get('trip', {}).get('routeId', 'N/A')
--- server/main.py
+++ server/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 
 setup_cors(app)
 app.include_router(api_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Bus Location API"}
