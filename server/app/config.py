54ae86c316951b2bfb7abd193af001d707a8638e
rulkimi <azrulkimi00@gmail.com>
2025-11-20 21:05:16 +0800

    fix: add email .env.example and for rulkimi origin

--- server/app/config.py
+++ server/app/config.py
@@ -9,7 +9,11 @@ origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
     "https://rulkimi.github.io",
+<<<<<<< HEAD
     "http://bus-location-tracker.rulkimi.com",
     "https://bus-location-tracker.rulkimi.com",
     "https://bus-location-server-pi.vercel.app",
+=======
+    "https://bus-location-tracker.rulkimi.com"
+>>>>>>> 9559aa5 (fix: add email .env.example and for rulkimi origin)
 ]
